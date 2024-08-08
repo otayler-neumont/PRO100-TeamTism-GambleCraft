508e13c3a624ef7d0378f2c1be134d9861cc2430
mastratton <mastratton@student.neumont.edu>
2024-08-08 17:43:11 -0600

    Made Custom Block

---
+++ src/main/java/com/welovemoney/gamblecraft/SlotsBlock.java
@@ -0,0 +1,25 @@
+package com.welovemoney.gamblecraft.blocks;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.BaseEntityBlock;
+import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+public class SlotsBlock extends BaseEntityBlock {
+    public SlotsBlock(Properties prop) {
+        super(prop);
+    }
+
+    @Nullable
+    @Override
+    public BlockEntity newBlockEntity(BlockPos blockPos, BlockState blockState) {
+        return null;
+    }
+
+    @Override
+    public RenderShape getRenderShape(BlockState blockState) {
+        return RenderShape.MODEL;
+    }
+}
