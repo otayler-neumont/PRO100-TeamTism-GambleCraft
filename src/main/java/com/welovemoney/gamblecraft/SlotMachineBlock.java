f398c3d304298e70f3e613866083b20c975e58c8
MrGlockenspiel <mrglockenspiel@protonmail.com>
2024-08-08 18:48:28 -0600

    Updated block related things, added en_US lang file

--- src/main/java/com/welovemoney/gamblecraft/SlotMachineBlock.java
+++ src/main/java/com/welovemoney/gamblecraft/SlotMachineBlock.java
@@ -1,20 +1,15 @@
 package com.welovemoney.gamblecraft;
 
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.EntityBlock;
 import net.minecraft.world.level.block.RenderShape;
 import net.minecraft.world.level.block.entity.BlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraftforge.event.entity.player.PlayerInteractEvent;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.jetbrains.annotations.Nullable;
-import org.spongepowered.asm.mixin.Implements;
 
-public class SlotsBlock extends BaseEntityBlock implements EntityBlock{
-    public SlotsBlock(Properties prop) {
+public class SlotMachineBlock extends BaseEntityBlock implements EntityBlock {
+    public SlotMachineBlock(Properties prop) {
         super(prop);
     }
 
