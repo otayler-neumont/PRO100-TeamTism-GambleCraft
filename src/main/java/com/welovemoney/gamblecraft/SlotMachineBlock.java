5d966b799fce1d927609826e4ca36f9109293050
romAcosta <romeroacosta992@gmail.com>
2024-08-13 18:46:18 -0600

    Output
    Blocks spits out an item

--- src/main/java/com/welovemoney/gamblecraft/SlotMachineBlock.java
+++ src/main/java/com/welovemoney/gamblecraft/SlotMachineBlock.java
@@ -5,15 +5,15 @@ import net.minecraft.network.chat.OutgoingChatMessage;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.BaseEntityBlock;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.EntityBlock;
-import net.minecraft.world.level.block.RenderShape;
+import net.minecraft.world.level.block.*;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.DispenserBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.BlockHitResult;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
@@ -21,8 +21,20 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import org.jetbrains.annotations.Nullable;
 import org.openjdk.nashorn.internal.objects.annotations.Function;
 import org.spongepowered.asm.mixin.Implements;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.item.Item;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 
-public class SlotMachineBlock extends BaseEntityBlock implements EntityBlock{
+
+public class SlotMachineBlock extends Block implements EntityBlock{
     public SlotMachineBlock(Properties prop) {
         super(prop);
     }
@@ -47,8 +59,19 @@ public class SlotMachineBlock extends BaseEntityBlock implements EntityBlock{
             if (!heldItem.isEmpty()) {
                 System.out.println("Item Held: " + heldItem);
                 heldItem.shrink(1);
+
+                ItemStack itemStack = new ItemStack(Items.CARROT);
+                dropItem(level,pos,itemStack);
+
             }
         }
         return InteractionResult.SUCCESS;
+    }
+
+    private void dropItem(Level level, BlockPos pos, ItemStack stack) {
+        // Create an ItemEntity and set its position
+        ItemEntity itemEntity = new ItemEntity(level, pos.getX() + 0.5, pos.getY() + 0.5, pos.getZ() + 0.5, stack);
+        itemEntity.setDefaultPickUpDelay();
+        level.addFreshEntity(itemEntity);
     }
 }
